88b71e64da65844bd28e8e1193ebd1e79f8d6b0d
sumitpotdar <sumitseems@gmail.com>
2020-05-28 21:03:56 -0500

    Add files via upload

---
+++ ARPsoftexport.ps1
@@ -0,0 +1,2 @@
+﻿$data = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$data| Export-Csv C:\Temp\ARPSoftware.csv -NoTypeInformation | Format-Table
